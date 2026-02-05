7c2047121a3e863b5f92e0378777cf94791305a9
Shiv Basak <sbasak@unc.edu>
2026-02-05 15:15:03 -0500

    Homework-3

---
+++ schools.js
@@ -0,0 +1,108 @@
+// triangle_hs.js
+// GeoJSON of Triangle-area high schools (12 features)
+// Coordinates are approximate; replace with verified coordinates if required.
+
+var triangleHighSchools = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Green Level High School",
+        "city": "Cary",
+        "description": "Known for strong academics and athletics in west Cary."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.8265, 35.7408] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Panther Creek High School",
+        "city": "Cary",
+        "description": "Large Wake County high school serving the Cary/Apex area."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.8540, 35.7712] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cary High School",
+        "city": "Cary",
+        "description": "One of the oldest high schools in Cary near downtown."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.7810, 35.7870] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Enloe Magnet High School",
+        "city": "Raleigh",
+        "description": "Magnet high school known for STEM, arts, and humanities programs."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.6160, 35.8018] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Broughton High School",
+        "city": "Raleigh",
+        "description": "Historic Raleigh high school located near downtown."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.6540, 35.7955] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Athens Drive High School",
+        "city": "Raleigh",
+        "description": "Southwest Raleigh high school with diverse programs and athletics."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.7000, 35.7405] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Durham School of the Arts",
+        "city": "Durham",
+        "description": "Arts-focused public school offering visual/performing arts tracks."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.9215, 35.9992] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hillside High School",
+        "city": "Durham",
+        "description": "Major high school serving southern Durham."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.9155, 35.9492] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Riverside High School",
+        "city": "Durham",
+        "description": "Northern Durham high school near the Eno River area."
+      },
+      "geometry": { "type": "Point", "coordinates": [-78.8965, 36.0620] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chapel Hill High School",
+        "city": "Chapel Hill",
+        "description": "CHCCS high school close to central Chapel Hill."
+      },
+      "geometry": { "type": "Point", "coordinates": [-79.0660, 35.9415] }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "East Chapel Hill High School",
+        "city": "Chapel Hill",
+        "description": "CHCCS high school serving east Chapel Hill."
+      },
+      "geometry": { "type": "Point", "coordinates": [-79.0030, 35.9392] }
+    }
+  ]
+};
